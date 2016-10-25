bd4a69516c97e9123421759c30516a9189e8da79
Daniel Goerz <dlg@lightwerk.com>
2016-10-25 17:05:51 +0200

    [BUGFIX] Add t3editor to dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,9 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '1.5.0',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '7.6.0-8.3.99',
-            'fluid_styled_content' => ''
+            'typo3' => '7.6.0-8.4.99',
+            'fluid_styled_content' => '',
+            't3editor' => ''
         ],
         'conflicts' => [],
         'suggests'  => [],
