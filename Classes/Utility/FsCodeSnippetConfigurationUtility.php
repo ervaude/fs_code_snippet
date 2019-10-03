3ca1d6c4cb27c3c463884f0d6a57cb9a5c48215e
Daniel Goerz <daniel.goerz@b13.com>
2019-10-03 12:43:47 +0200

    [TASK] Streamline doc header comment

--- Classes/Utility/FsCodeSnippetConfigurationUtility.php
+++ Classes/Utility/FsCodeSnippetConfigurationUtility.php
@@ -1,18 +1,14 @@
 <?php
 namespace DanielGoerz\FsCodeSnippet\Utility;
 
-/**
- * This file is part of the TYPO3 CMS project.
+/*
+ * This file is part of TYPO3 CMS-based extension fs_code_snippet.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
  */
+
 use DanielGoerz\FsCodeSnippet\Enumeration\CodeSnippetLanguage;
 
 /**
