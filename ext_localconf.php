77ded8af717b681c0a9f83145529229394e34959
Daniel Goerz <daniel.goerz@b13.com>
2019-10-03 11:19:19 +0200

    [TASK] Use namespace imports in ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,11 @@
 <?php
 defined('TYPO3_MODE') || exit('Access denied.');
 
+use DanielGoerz\FsCodeSnippet\Form\Element\CodeSnippetElement;
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 if (TYPO3_MODE === 'BE') {
     call_user_func(
         function () {
@@ -8,20 +13,20 @@ if (TYPO3_MODE === 'BE') {
             $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['fs_code_snippet']
                 = \DanielGoerz\FsCodeSnippet\Hook\CodeSnippetPreviewRenderer::class;
 
-            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(IconRegistry::class);
             $iconRegistry->registerIcon(
                 'fs-code-snippet',
-                \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+                BitmapIconProvider::class,
                 ['source' => 'EXT:fs_code_snippet/Resources/Public/Images/code-snippet-icon.png']
             );
 
             $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1449747562] = array(
                 'nodeName' => 'fs_code_snippet',
                 'priority' => 50,
-                'class' => \DanielGoerz\FsCodeSnippet\Form\Element\CodeSnippetElement::class,
+                'class' => CodeSnippetElement::class,
             );
 
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fs_code_snippet/Configuration/PageTSconfig/PageTSConfig.tsconfig">');
+            ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fs_code_snippet/Configuration/PageTSconfig/PageTSConfig.tsconfig">');
         }
     );
 }
