208aad7659b0b18309bb62bb7ee03a442c3e9f6a
Daniel Goerz <daniel.goerz@b13.com>
2021-11-22 18:58:16 +0100

    [TASK] Make installable in TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
             'fluid_styled_content' => '',
             't3editor' => ''
         ],
